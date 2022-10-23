034082e736b017dc6390054598b59b8b3decb56b
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-10-23 15:46:01 +0530

    feat: add Sketch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import Boards from "./pages/Boards";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
+import { CanvasProvider } from "./contexts/CanvasContext";
+import Sketch from "./pages/Sketch";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +23,13 @@ function App() {
                 <Route path="/" exact component={Register} />
                 <Route path="/login" exact component={Login} />
                 <PrivateRoute path="/sketches" exact component={Boards} />
+                <CanvasProvider>
+                  <PrivateRoute
+                    path="/sketches/:sketchId"
+                    exact
+                    component={Sketch}
+                  />
+                </CanvasProvider>
               </Switch>
             </Router>
             <ReactQueryDevtools initialIsOpen={false} />
