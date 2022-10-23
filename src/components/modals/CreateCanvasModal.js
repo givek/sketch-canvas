119073842678fbd8603e14d48e37c0cde00f9617
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-10-23 15:30:54 +0530

    feat: add CreateCanvasModal

---
+++ src/components/modals/CreateCanvasModal.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  ModalFooter,
+} from "@chakra-ui/modal";
+import { Button } from "@chakra-ui/button";
+import { Formik, Form } from "formik";
+import { FormikControl } from "../FormikControl";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useHistory } from "react-router-dom";
+
+const initialValues = {
+  name: "",
+};
+
+export const CreateCanvasModal = (props) => {
+  const axiosPrivate = useAxiosPrivate();
+  const queryClient = useQueryClient();
+  const histroy = useHistory();
+  const createNewCanvasQuery = useMutation(
+    (canvas) => axiosPrivate.post(`api/canvas`, canvas),
+    {
+      onSuccess: (data) => {
+        props.onClose();
+        histroy.push(`/sketches/${data.data._id}`);
+        return queryClient.setQueriesData(["canvases"], (oldQueryData) => {
+          return { ...oldQueryData, data: [...oldQueryData.data, data.data] };
+        });
+      },
+    }
+  );
+
+  const onSumbit = async (data = {}, { setErrors }) => {
+    console.log(`form data`, data.name);
+
+    try {
+      createNewCanvasQuery.mutate(data);
+    } catch (error) {
+      // TODO: handle errors
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.data.name);
+      }
+      alert(error);
+    }
+  };
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalOverlay />
+      <Formik initialValues={initialValues} onSubmit={onSumbit}>
+        <Form>
+          <ModalContent>
+            <ModalHeader fontWeight="medium">Canvas name</ModalHeader>
+            <ModalBody>
+              <FormikControl
+                control="input"
+                type="text"
+                name="name"
+                label="Canvas name"
+                size="sm"
+                placeholder="Naya"
+                helpText="Choose something familiar like your team name."
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button size="sm" variant="ghost" mr={3} onClick={props.onClose}>
+                Close
+              </Button>
+              <Button type="submit" size="sm" colorScheme="blue">
+                Create
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Form>
+      </Formik>
+    </Modal>
+  );
+};
