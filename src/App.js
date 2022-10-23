0671fd192e7b63c02a4107f786e39f5457594129
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-10-23 15:31:05 +0530

    feat: add Boards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthProvider";
+import PrivateRoute from "./components/PrivateRoute";
+import Boards from "./pages/Boards";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
         <AuthProvider>
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Register} />{" "}
-              <Route path="/login" exact component={Login} />
-            </Switch>
-          </Router>
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <Switch>
+                <Route path="/" exact component={Register} />
+                <Route path="/login" exact component={Login} />
+                <PrivateRoute path="/sketches" exact component={Boards} />
+              </Switch>
+            </Router>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </AuthProvider>
       </ChakraProvider>
     </div>
